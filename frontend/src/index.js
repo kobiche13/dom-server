2bbc66e99a8a9a4d46f1d5cd5c216a8ee53d5dc8
kobiche <koba156@yandex.ru>
2025-05-09 23:20:28 +0300

    v8

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 // Сюда импортируются файлы приложения
 
-import LoadTask from './js/loadTask.js'
+
 import '../src/main.css'
-import AddNewTicket from './js/addNewTicket.js'
+
 import AppTask from './app.js'
 
 let runApp = new AppTask();
